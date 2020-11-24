08f80093f45bf3832f7695c30a4eedf351663842
dsiervo <emetdan@gmail.com>
2020-11-24 13:05:04 -0500

    Creating the branch

---
+++ utils/origins_pruning.py
@@ -0,0 +1,13 @@
+#!/home/dsiervo/anaconda3/envs/pnet/bin/python
+# -*- coding: utf-8 -*-
+"""
+Created on Nov 24 13:01:33 2020
+
+@author: Daniel Siervo, emetdan@gmail.com
+"""
+
+class xml_origins(object):
+    """
+    docstring
+    """
+    pass
